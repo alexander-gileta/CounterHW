8fcdbda2227f8e024235c7633609ab428bb1d6da
Alex <alexander.gileta@yandex.ru>
2025-11-12 10:51:45 +0300

    Создано приложение-счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    let dateFormatter = DateFormatter()
+    
+    private var counter = 0
+    
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterView: UILabel!
+    @IBOutlet weak var counterHistoryView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        dateFormatter.dateFormat = "dd.MM.yy в HH:mm:ss"
+    }
+    
+    @IBAction func buttonPlusDidTap() {
+        if counter < Int.max {
+            counter+=1
+            counterHistoryView.text.append("\n\(dateFormatter.string(from: Date())): значение изменено на +1")
+        }
+        counterView.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func buttinMinusDidTap() {
+        if counter > 0 {
+            counter-=1
+            counterHistoryView.text.append("\n\(dateFormatter.string(from: Date())): значение изменено на -1")
+        } else {
+            counterHistoryView.text.append("\n\(dateFormatter.string(from: Date())): попытка уменьшить значение счётчика ниже 0")
+        }
+        counterView.text = "Значение счётчика: \(counter)"
     }
 
-
+    @IBAction func buttonResetDidTap() {
+        counter = 0
+        counterHistoryView.text.append("\n\(dateFormatter.string(from: Date())): значение сброшено")
+        counterView.text = "Значение счётчика: \(counter)"
+    }
 }
 
